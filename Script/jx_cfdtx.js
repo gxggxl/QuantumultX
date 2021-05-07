58a8f44f86610a1a083bb1748e24fb171658e7c8
gxggxl <gxggxl@qq.com>
2021-05-07 09:48:35 +0800

    Update jx_cfdtx.js

--- Script/jx_cfdtx.js
+++ Script/jx_cfdtx.js
@@ -39,7 +39,7 @@ const JD_API_HOST = "https://m.jingxi.com/";
 const jdCookieNode = $.isNode() ? require("./jdCookie.js") : "";
 const jdTokenNode = $.isNode() ? require('./jdJxncTokens.js') : '';
 // 循还次数(多号并发)
-const CY = 3;
+const CY = 2;
 $.result = [];
 $.cookieArr = [];
 $.currentCookie = '';
